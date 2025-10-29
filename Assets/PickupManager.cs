c092941e673f5dbc302a3198ba2253abdbea2883
Michael <hubu99@gmail.com>
2025-10-29 11:43:15 -0300

    Coin Rotating & Bouncing
    - Gave the coins a lil flare by making them bounce and rotate

--- Assets/PickupManager.cs
+++ Assets/PickupManager.cs
@@ -9,18 +9,21 @@ public class PickupManager : MonoBehaviour
     private float acceleration = 15f;
     private GameObject player;
     private Vector3 defaultScale;
+    private Vector3 centerPosition;
+    private float bounceIntensity = 0.2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         defaultScale = transform.localScale;
+        centerPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isPickedUp) 
+        if (isPickedUp)
         {
             float step = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
@@ -35,12 +38,18 @@ public class PickupManager : MonoBehaviour
 
             Debug.Log($"Distance: {distance} and scale: {scaleMultiplier}");
 
-            if (distance < 0.3f && !collected) 
+            if (distance < 0.3f && !collected)
             {
                 collected = true;
                 player.GetComponent<PlayerController>().PickupObject();
                 Destroy(gameObject);
             }
+        }
+        else 
+        {
+            transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+
+            transform.position = centerPosition + new Vector3(0, Mathf.Sin(Time.time) * bounceIntensity);
         }
     }
 }
